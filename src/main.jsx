773731989e708a143385aecb2e5ed0bd2522b10a
ShooterCrow <cydollarsign1@gmail.com>
2026-01-04 14:08:36 +0100

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { HelmetProvider } from 'react-helmet-async'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+// Only disable devtools in production
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
+
+// Create a data router
+const router = createBrowserRouter([
+  {
+    path: '/*',
+    element: <App />,
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  </StrictMode>
 )
