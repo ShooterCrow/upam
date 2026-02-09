362624b20ed0e596f7420852f362465613a26557
ShooterCrow <cydollarsign1@gmail.com>
2026-02-09 15:38:42 +0100

    Update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -764,13 +764,6 @@ const Index = () => {
                                         />
                                     </div>
 
-                                    {/* Position Badge */}
-                                    <div className="absolute top-4 left-4">
-                                        <div className="px-3 py-1.5 bg-[#EB010C] text-white text-xs font-medium rounded-full">
-                                            #{leader.id}
-                                        </div>
-                                    </div>
-
                                     {/* Information overlay - Partially visible by default, fully visible on hover */}
                                     <div className="absolute inset-0 bg-gradient-to-t from-black/50 via-black/10 to-transparent group-hover:from-black/80 group-hover:via-black/40 transition-all duration-300">
                                         <div className="absolute bottom-0 left-0 right-0 p-4 lg:p-6 flex">
